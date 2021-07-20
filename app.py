d38d89d0aba46d2bc532232694df842f160cf1c9
Lester Huang <huangl302d@gmail.com>
2021-07-19 22:22:48 -0400

    moved source to rss

--- app.py
+++ app.py
@@ -1,27 +1,53 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import os
+import requests
+import bs4
 
-# Run selenium in headless mode
-opts = Options()
-opts.add_argument(" --headless")
+res = requests.get(
+    'https://trends.google.com/trends/trendingsearches/daily/rss?geo=US')
+trends = dict()
 
-# Path to browser's exec
-opts.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe"
-# Path to web driver 
-chrome_driver = os.getcwd() + "\\chromedriver.exe"
+'''
+Converts from XML tags to dictionary-friendly keys
+Also used to filter out unwanted entries
+'''
+tagToKey = {
+    'title': 'title',
+    'ht:approx_traffic': 'traffic',
+    'description': 'desc',
+    'ht:picture': 'pictureSrc',
+    'ht:news_item': 'newsItem',
+    'ht:news_item_title': 'newsItemTitle',
+    'ht:news_item_snippet': 'newsItemDesc',
+    'ht:news_item_url': 'newsItemURL',
+}
 
-# Scrapes url w/ dynamic JS
-driver = webdriver.Chrome(options=opts, executable_path=chrome_driver)
-driver.get("https://trends.google.com/trends/trendingsearches/daily?geo=US")
 
-# Loads into BeautifulSoup object
-soup_file = driver.page_source
-soup = BeautifulSoup(soup_file, features="html.parser")
+def soup2dict(list):
+    if len(list) == 0:  # Default case
+        return
 
-# Scrape like static page
-print(soup.title)
+    newDict = dict()
+    for tag in list:
+        if tag.name in tagToKey:  # Only includes values we need
+            newDict[tagToKey[tag.name]] = soup2dict(tag.contents)
+    return newDict
 
-# Closes chrome driver
-driver.quit()
+
+if res.status_code == 200:
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    items = soup.find_all('item')
+
+    for item in items:
+        trends[item.title] = soup2dict(item.contents)
+
+    trends = soup2dict(items)
+    print(trends)
+    # print(soup.prettify)
+    # Loops through each item in the list
+    # for item in soup.find_all('item'):
+    # trends[item.title] = {
+    # 	'traffic': item.approx_traffic,
+    # 	'description': item.description,
+    # 	'pictureSource': item.picture,
+    # }
+else:
+    print(f"Error: Status code {res.status_code})")
